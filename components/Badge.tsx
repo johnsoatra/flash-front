2791d52983646934c1ed796c40435f09919fd7c1
soatra <soatra206.infinity@gmail.com>
2026-04-04 16:12:39 +0700

    design some page

---
+++ components/Badge.tsx
@@ -0,0 +1,11 @@
+export default function Badge({
+  amount,
+}: {
+  amount: number;
+}) {
+  return (
+    <span className="w-4 h-4 flex justify-center items-center bg-danger rounded-full">
+      <span className="text-xs font-medium text-white">{amount}</span>
+    </span>
+  );
+}
---
+++ layout/Header.tsx
@@ -0,0 +1,11 @@
+export default function Header() {
+  return (
+    <header
+      className="w-full min-h-18 flex items-center justify-center bg-back border-b">
+      <div className="flex items-center justify-between gap-x-1.5">
+        <h2 className="font-bold tracking-[25%]">FLASH</h2>
+        
+      </div>
+    </header>
+  );
+}
