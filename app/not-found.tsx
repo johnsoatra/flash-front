49c81123842c058e32384d802356c5cab56238f7
soatra <soatra206.infinity@gmail.com>
2026-04-23 11:42:33 +0700

    add not found page

---
+++ app/not-found.tsx
@@ -0,0 +1,7 @@
+export default function NotFound() {
+  return (
+    <div className="w-full flex-1 flex items-center justify-center">
+      <h1 className="text-xl text-center">404 - Page Not Found</h1>
+    </div>
+  );
+}
